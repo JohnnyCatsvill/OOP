1cad87d02fff09bcc60147206ef956fbdc6784de
Johnny Catsville <>
2020-06-14 03:23:48 +0300

    Now test use files directly from main program

--- Lab3_Task1_Car_Program/Car.cpp
+++ Lab3_Task1_Car_Program/Car.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void ClearSpaces(std::string &str)
+void ClearSpaces(std::string& str)
 {
     for (int i = 0; i < str.length(); i++)
     {
@@ -16,7 +16,7 @@ void ClearSpaces(std::string &str)
     }
 }
 
-int DemandNumber(const string &demandMessage, std::string strInt)
+int DemandNumber(const string& demandMessage, std::string strInt)
 {
     while (!(strInt[0] >= '0' && strInt[0] <= '9' || strInt[0] == '-' && strInt[1] >= '0' && strInt[1] <= '9'))
     {
@@ -26,7 +26,7 @@ int DemandNumber(const string &demandMessage, std::string strInt)
     return stoi(strInt);
 }
 
-string GetGearTrouble(CCar &car, int gear)
+string GetGearTrouble(CCar& car, int gear)
 {
     if (car.GetMotionSide() != 0 && gear == -1)
     {
@@ -62,11 +62,11 @@ string GetGearTrouble(CCar &car, int gear)
     }
 }
 
-string GetSpeedTrouble(CCar &car, int speed)
+string GetSpeedTrouble(CCar& car, int speed)
 {
     if (speed < 0)
     {
-    return "Скорость должна быть неотрицательной";
+        return "Скорость должна быть неотрицательной";
     }
     else if ((car.GetCurrentGear() == -1 && (0 > speed || speed > 20)) ||
         (car.GetCurrentGear() == 1 && (0 > speed || speed > 30)) ||
@@ -81,7 +81,7 @@ string GetSpeedTrouble(CCar &car, int speed)
     {
         return "Двигатель машины заглушен";
     }
-    
+
     else if (car.GetCurrentGear() == 0 && car.GetCurrentSpeed() < speed)
     {
         return "На текущей передаче нельзя разгоняться";
@@ -92,6 +92,65 @@ string GetSpeedTrouble(CCar &car, int speed)
     }
 }
 
+void ProcessCommand(std::string& command, CCar& car)
+{
+    if (command == "Info")
+    {
+        cout << "Состояние двигателя - " << (car.IsEngineOn() ? "Включен" : "Выключен") << '\n';
+        cout << "Направление движения - " << ((car.GetMotionSide() == 0) ? "Стоит на месте" : (car.GetMotionSide() > 0) ? "Вперёд" : "Назад") << '\n';
+        cout << "Текущая скорость - " << car.GetCurrentSpeed() << '\n';
+        cout << "Текущая передача - " << car.GetCurrentGear() << '\n';
+    }
+    else if (command == "EngineOn")
+    {
+        cout << (car.TurnOnEngine() ? "Машина успешно заведена" : "Ошибка в попытке завести машину") << '\n';
+    }
+    else if (command == "EngineOff")
+    {
+        cout << (car.TurnOffEngine() ? "Машина успешно затихла" : "Ошибка в попытке заглушить двигатель") << '\n';
+    }
+    else if (command.find("SetGear") == 0)
+    {
+        string strInt = command;
+        strInt.erase(0, 7);
+        ClearSpaces(strInt);
+
+        int gear = DemandNumber("Введите передачу\n", strInt);
+
+        if (car.SetGear(gear))
+        {
+            cout << "Передача успешно изменена на " << gear << '\n';
+        }
+        else
+        {
+            cout << "Ошибка в попытке изменить передачу\n";
+            cout << GetGearTrouble(car, gear) << endl;
+        }
+    }
+    else if (command.find("SetSpeed") == 0)
+    {
+        string strInt = command;
+        strInt.erase(0, 8);
+        ClearSpaces(strInt);
+
+        int speed = DemandNumber("Введите скорость\n", strInt);
+
+        if (car.SetSpeed(speed))
+        {
+            cout << "Скорость успешно изменена на " << speed << '\n';
+        }
+        else
+        {
+            cout << "Ошибка в попытке изменить скорость\n";
+            cout << GetSpeedTrouble(car, speed) << endl;
+        }
+    }
+    else
+    {
+        cout << "Команда не распознана\n";
+    }
+}
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
@@ -99,60 +158,6 @@ int main()
     std::string command;
     while (std::getline(std::cin, command))
     {
-        if (command == "Info")
-        {
-            cout << "Состояние двигателя - " << (car.IsEngineOn() ? "Включен" : "Выключен") << '\n';
-            cout << "Направление движения - " << ((car.GetMotionSide() == 0) ? "Стоит на месте" : (car.GetMotionSide() > 0) ? "Вперёд" : "Назад") << '\n';
-            cout << "Текущая скорость - " << car.GetCurrentSpeed() << '\n';
-            cout << "Текущая передача - " << car.GetCurrentGear() << '\n';
-        }
-        else if (command == "EngineOn")
-        {
-            cout << (car.TurnOnEngine() ? "Машина успешно заведена" : "Ошибка в попытке завести машину") << '\n';
-        }
-        else if (command == "EngineOff")
-        {
-            cout << (car.TurnOffEngine() ? "Машина успешно затихла" : "Ошибка в попытке заглушить двигатель") << '\n';
-        }
-        else if (command.find("SetGear") == 0)
-        {
-            string strInt = command;
-            strInt.erase(0, 7);
-            ClearSpaces(strInt);
-
-            int gear = DemandNumber("Введите передачу\n", strInt);
-
-            if (car.SetGear(gear))
-            {
-                cout << "Передача успешно изменена на " << gear << '\n';
-            }
-            else
-            {
-                cout << "Ошибка в попытке изменить передачу\n";
-                cout << GetGearTrouble(car, gear) << endl;
-            }
-        }
-        else if (command.find("SetSpeed") == 0)
-        {
-            string strInt = command;
-            strInt.erase(0, 8);
-            ClearSpaces(strInt);
-            
-            int speed = DemandNumber("Введите скорость\n", strInt);
-
-            if (car.SetSpeed(speed))
-            {
-                cout << "Скорость успешно изменена на " << speed << '\n';
-            }
-            else
-            {
-                cout << "Ошибка в попытке изменить скорость\n";
-                cout << GetSpeedTrouble(car, speed) << endl;
-            }
-        }
-        else
-        {
-            cout << "Команда не распознана\n";
-        }
+        ProcessCommand(command, car);
     }
 }
---
+++ Lab3_Task1_Car_Tests/tests.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include "../Lab3_Task1_Car_Program/CCar.h"
+#include <assert.h>
+
+using namespace std;
+
+void TestCarCreation()
+{
+    CCar car;
+    assert(car.IsEngineOn() == false);
+    assert(car.GetMotionSide() == 0);
+    assert(car.GetCurrentSpeed() == 0);
+    assert(car.GetCurrentGear() == 0);
+}
+
+void TestCarNormalDriving()
+{
+    CCar car;
+    assert(car.TurnOnEngine()); //заводимся
+    assert(car.SetGear(1));  //разгоняемся
+    assert(car.SetSpeed(25));
+    assert(car.SetGear(2));
+    assert(car.SetSpeed(40));
+    assert(car.SetGear(3));
+    assert(car.SetSpeed(50)); 
+    assert(car.SetSpeed(40)); //притормаживаем
+    assert(car.SetGear(2)); //уменьшаем передачу
+    assert(car.SetGear(0)); //переходим на нейтралку
+    assert(car.SetSpeed(0)); //тормозим
+    assert(car.SetGear(-1)); //даем задний ход
+    assert(car.SetSpeed(10));
+    assert(car.SetSpeed(0)); //тормозим
+    assert(car.SetGear(0)); //нейтралка
+    assert(car.TurnOffEngine()); //глушим движок
+    assert(car.IsEngineOn() == false); //машина как новая
+    assert(car.GetMotionSide() == 0);
+    assert(car.GetCurrentSpeed() == 0);
+    assert(car.GetCurrentGear() == 0);
+}
+
+void TestCarWithWrongGears()
+{
+    CCar car;
+    assert(!car.SetGear(1)); //машина не заведена
+    car.TurnOnEngine();
+    assert(!car.SetGear(2)); //машина стоит
+    car.SetGear(1);
+    car.SetSpeed(5);
+    assert(!car.SetGear(2)); //машине не хватает скорости
+    assert(!car.SetGear(6)); //такой передачи нет
+    assert(!car.SetGear(-1)); //машина едет вперед
+    car.SetSpeed(0);
+    car.SetGear(-1);
+    assert(!car.SetGear(-2)); //такой передачи нет
+    car.SetSpeed(5);
+    assert(!car.SetGear(1)); //машина едет назад
+    car.SetGear(0);
+    car.SetSpeed(4);
+    assert(!car.SetGear(1)); //машина все еще едет назад
+}
+
+void TestCarWithWrongSpeeds()
+{
+    CCar car;
+    assert(!car.SetSpeed(5)); //машина не заведена
+    car.TurnOnEngine();
+    car.SetGear(1);
+    assert(!car.SetSpeed(-5)); //таких скоростей нет
+    assert(!car.SetSpeed(40)); //скорость выше допустимой на текущей передаче
+    car.SetSpeed(20);
+    car.SetGear(0);
+    assert(!car.SetSpeed(25)); //на нейтралке разгоняться нельзя
+    car.SetGear(1);
+    car.SetSpeed(25);
+    car.SetGear(2);
+    assert(!car.SetSpeed(10)); //скорость ниже допустимой на текущей передаче
+}
+
+void TestCarWrongTurnOnsOffs()
+{
+    CCar car;
+    assert(!car.TurnOffEngine()); //был и так выключен
+    car.TurnOnEngine();
+    assert(!car.TurnOnEngine()); //был и так включен
+    car.SetGear(1);
+    assert(!car.TurnOffEngine()); //передача не нейтральная
+    car.SetSpeed(5);
+    car.SetGear(0);
+    assert(!car.TurnOffEngine()); //скорость не нулевая
+}
+
+int main()
+{
+    TestCarCreation();
+    TestCarNormalDriving();
+    TestCarWithWrongGears();
+    TestCarWithWrongSpeeds();
+    TestCarWrongTurnOnsOffs();
+
+    return 0;
+}
